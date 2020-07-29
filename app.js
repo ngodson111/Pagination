c5caedbc240ec81745fd3b03d2bcb3c2b531d613
ngodson111 <narannpn@gmail.com>
2020-07-29 14:16:47 +0545

    Initial Commit

---
+++ app.js
@@ -0,0 +1,220 @@
+let data = [
+  { id: 1, name: "narayan" },
+  { id: 2, name: "ngodson" },
+  { id: 3, name: "naresh" },
+  { id: 4, name: "bimal" },
+  { id: 5, name: "irish" },
+  { id: 6, name: "santosh" },
+  { id: 7, name: "sameer" },
+  { id: 8, name: "rishat" },
+  { id: 8, name: "narayan" },
+  { id: 10, name: "ngodson" },
+  { id: 11, name: "naresh" },
+  { id: 12, name: "bimal" },
+  { id: 13, name: "irish" },
+  { id: 14, name: "santosh" },
+  { id: 15, name: "sameer" },
+  { id: 16, name: "rishat" },
+  { id: 17, name: "narayan" },
+  { id: 18, name: "ngodson" },
+  { id: 19, name: "naresh" },
+  { id: 20, name: "bimal" },
+  { id: 21, name: "irish" },
+  { id: 22, name: "santosh" },
+  { id: 23, name: "sameer" },
+  { id: 24, name: "rishat" },
+  { id: 25, name: "rishat" },
+  { id: 26, name: "rishat" },
+  { id: 27, name: "rishat" },
+  { id: 28, name: "rishat" },
+  { id: 29, name: "rishat" },
+  { id: 30, name: "rishat" },
+  { id: 31, name: "rishat" },
+  { id: 32, name: "rishat" },
+  { id: 33, name: "rishat" },
+  { id: 34, name: "rishat" },
+  { id: 35, name: "rishat" },
+  { id: 1, name: "narayan" },
+  { id: 2, name: "ngodson" },
+  { id: 3, name: "naresh" },
+  { id: 4, name: "bimal" },
+  { id: 5, name: "irish" },
+  { id: 6, name: "santosh" },
+  { id: 7, name: "sameer" },
+  { id: 8, name: "rishat" },
+  { id: 8, name: "narayan" },
+  { id: 10, name: "ngodson" },
+  { id: 11, name: "naresh" },
+  { id: 12, name: "bimal" },
+  { id: 13, name: "irish" },
+  { id: 14, name: "santosh" },
+  { id: 15, name: "sameer" },
+  { id: 16, name: "rishat" },
+  { id: 17, name: "narayan" },
+  { id: 18, name: "ngodson" },
+  { id: 19, name: "naresh" },
+  { id: 20, name: "bimal" },
+  { id: 21, name: "irish" },
+  { id: 22, name: "santosh" },
+  { id: 23, name: "sameer" },
+  { id: 24, name: "rishat" },
+  { id: 25, name: "rishat" },
+  { id: 26, name: "rishat" },
+  { id: 27, name: "rishat" },
+  { id: 28, name: "rishat" },
+  { id: 29, name: "rishat" },
+  { id: 30, name: "rishat" },
+  { id: 31, name: "rishat" },
+  { id: 32, name: "rishat" },
+  { id: 33, name: "rishat" },
+  { id: 34, name: "rishat" },
+  { id: 35, name: "rishat" },
+  { id: 1, name: "narayan" },
+  { id: 2, name: "ngodson" },
+  { id: 3, name: "naresh" },
+  { id: 4, name: "bimal" },
+  { id: 5, name: "irish" },
+  { id: 6, name: "santosh" },
+  { id: 7, name: "sameer" },
+  { id: 8, name: "rishat" },
+  { id: 8, name: "narayan" },
+  { id: 10, name: "ngodson" },
+  { id: 11, name: "naresh" },
+  { id: 12, name: "bimal" },
+  { id: 13, name: "irish" },
+  { id: 14, name: "santosh" },
+  { id: 15, name: "sameer" },
+  { id: 16, name: "rishat" },
+  { id: 17, name: "narayan" },
+  { id: 18, name: "ngodson" },
+  { id: 19, name: "naresh" },
+  { id: 20, name: "bimal" },
+  { id: 21, name: "irish" },
+  { id: 22, name: "santosh" },
+  { id: 23, name: "sameer" },
+  { id: 24, name: "rishat" },
+  { id: 25, name: "rishat" },
+  { id: 26, name: "rishat" },
+  { id: 27, name: "rishat" },
+  { id: 28, name: "rishat" },
+  { id: 29, name: "rishat" },
+  { id: 30, name: "rishat" },
+  { id: 31, name: "rishat" },
+  { id: 32, name: "rishat" },
+  { id: 33, name: "rishat" },
+  { id: 34, name: "rishat" },
+  { id: 35, name: "rishat" },
+  { id: 1, name: "narayan" },
+  { id: 2, name: "ngodson" },
+  { id: 3, name: "naresh" },
+  { id: 4, name: "bimal" },
+  { id: 5, name: "irish" },
+  { id: 6, name: "santosh" },
+  { id: 7, name: "sameer" },
+  { id: 8, name: "rishat" },
+  { id: 8, name: "narayan" },
+  { id: 10, name: "ngodson" },
+  { id: 11, name: "naresh" },
+  { id: 12, name: "bimal" },
+  { id: 13, name: "irish" },
+  { id: 14, name: "santosh" },
+  { id: 15, name: "sameer" },
+  { id: 16, name: "rishat" },
+  { id: 17, name: "narayan" },
+  { id: 18, name: "ngodson" },
+  { id: 19, name: "naresh" },
+  { id: 20, name: "bimal" },
+  { id: 21, name: "irish" },
+  { id: 22, name: "santosh" },
+  { id: 23, name: "sameer" },
+  { id: 24, name: "rishat" },
+  { id: 25, name: "rishat" },
+  { id: 26, name: "rishat" },
+  { id: 27, name: "rishat" },
+  { id: 28, name: "rishat" },
+  { id: 29, name: "rishat" },
+  { id: 30, name: "rishat" },
+  { id: 31, name: "rishat" },
+  { id: 32, name: "rishat" },
+  { id: 33, name: "rishat" },
+  { id: 34, name: "rishat" },
+  { id: 35, name: "rishat" },
+];
+
+let state = {
+  querySet: data,
+  page: 1,
+  rows: 10,
+  window: 5,
+};
+
+function Pagination(querySet, page, rows) {
+  let trimstart = (page - 1) * rows;
+  let trimend = trimstart + rows;
+  let trimmedData = querySet.slice(trimstart, trimend);
+  let pages = Math.ceil(querySet.length / rows);
+  return {
+    querySet: trimmedData,
+    pages: pages,
+  };
+}
+
+function pageBtn(pages) {
+  let wrapper = document.querySelector(".pagination-wrapper");
+  wrapper.innerHTML = "";
+
+  let maxLeft = state.page - Math.floor(state.window / 2);
+  let maxRight = state.page + Math.floor(state.window / 2);
+  if (maxLeft < 1) {
+    maxLeft = 1;
+    maxRight = state.window;
+  }
+  if (maxRight > pages) {
+    maxLeft = pages - (state.window - 1);
+    maxRight = pages;
+    if (maxLeft < 1) {
+      maxLeft = 1;
+    }
+  }
+  for (let page = maxLeft; page <= maxRight; page++) {
+    wrapper.innerHTML += `<button value='${page}' class = 'page'>${page}</button>`;
+  }
+
+  if (state.page != 1) {
+    wrapper.innerHTML =
+      `<button value='1' class = 'page'>&#171;</button>` + wrapper.innerHTML;
+  }
+  if (state.page != pages) {
+    wrapper.innerHTML += `<button value='${pages}' class = 'page'>&#187;</button>`;
+  }
+
+  //eventhandeling
+  for (let k = 0; k < wrapper.children.length; k++) {
+    wrapper.children[k].addEventListener("click", (e) => {
+      document.querySelector("#parent").innerHTML = "";
+      state.page = parseInt(e.target.value);
+      //again build
+      buildData();
+    });
+  }
+}
+
+function buildData() {
+  let element = document.querySelector("#parent");
+
+  //pagination function
+  let data = Pagination(state.querySet, state.page, state.rows);
+
+  list = data.querySet;
+  for (let b = 0; b < list.length; b++) {
+    let row = `<li>${list[b].name}</li>`;
+    element.innerHTML += row;
+  }
+
+  //button
+  pageBtn(data.pages);
+}
+
+buildData();
+
+// This code official by narayan neupane
